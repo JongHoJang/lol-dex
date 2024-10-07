a4313d3168ccd314b633c2b80a2e137e7644556b
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-10-08 00:53:53 +0900

    빌드 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             리그 오브 레전드 정보 앱
           </h1>
           <p className="mb-20">
-            Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.
+            Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다!
           </p>
         </div>
 

f2cfeed1c8e11c5b1e9c4c98d32b56f523356224
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-10-08 00:50:34 +0900

    ReadMe 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,41 +13,52 @@ export default function Home() {
     <>
       <div className="flex flex-col justify-center items-center">
         <div>
-          <h1 className="flex justify-center my-10 text-[40px] font-bold">
+          <h1 className="flex justify-center mt-20 mb-5 text-[40px] font-bold">
             리그 오브 레전드 정보 앱
           </h1>
-          <p>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</p>
+          <p className="mb-20">
+            Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.
+          </p>
         </div>
 
         <div className="flex flex-col">
-          <Link href={"/champions"}>
+          <Link href={"/champions"} className="relative inline-block mb-10">
             <Image
               src={`https://img.danawa.com/cp_images/service/79/3428645/150172432401670489095.jpg`}
               alt={`챔피언 사진`}
               width={500}
               height={300}
+              className="w-full h-auto "
             />
-            챔피온 목록 보기
+            <span className="absolute inset-0 flex items-center justify-center text-white font-bold text-lg bg-black bg-opacity-50">
+              챔피온 목록 보기
+            </span>
           </Link>
 
-          <Link href={"/items"}>
+          <Link href={"/items"} className="relative inline-block mb-10">
             <Image
               src={`https://i3.ruliweb.com/ori/20/11/11/175b5b92d4134a3fd.jpeg`}
               alt={`챔피언 사진`}
               width={500}
               height={300}
+              className="w-full h-auto"
             />
-            아이템 목록 보기
+            <span className="absolute inset-0 flex items-center justify-center text-white font-bold text-lg bg-black bg-opacity-50">
+              아이템 목록 보기
+            </span>
           </Link>
 
-          <Link href={"/rotation"}>
+          <Link href={"/rotation"} className="relative inline-block mb-10">
             <Image
               src={`https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F2161543A539F26491A`}
               alt={`챔피언 사진`}
               width={500}
               height={300}
+              className="w-full h-auto"
             />
-            로테이션 챔피언 목록 보기
+            <span className="absolute inset-0 flex items-center justify-center text-white font-bold text-lg bg-black bg-opacity-50">
+              로테이션 챔피언 목록 보기
+            </span>
           </Link>
         </div>
       </div>
